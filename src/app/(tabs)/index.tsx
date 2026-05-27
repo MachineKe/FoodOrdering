ce7fc4c553ec54762c7673ef724e85708d16557f
machineke <markarapsoi72@gmail.com>
2026-05-27 15:18:35 +0300

    add expo router screen

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,18 +1,6 @@
-import products from '@/assets/data/products';
-import { FlatList } from 'react-native';
-import ProductListItem from '@/src/components/ProductListItem';
 
+import { Redirect } from "expo-router";
 
-
-export default function MenuScreen() {
-  return (
-    <FlatList
-      data={products}
-      renderItem={({ item }) => <ProductListItem product={item} />}
-      numColumns={2}
-      contentContainerStyle={{ gap: 10, padding: 10 }}
-      columnWrapperStyle={{ gap: 10 }}
-    />
-  );
+export default function TabIndex() {
+    return <Redirect href="/menu" />;
 }
-
---
+++ src/app/(tabs)/menu/[id].tsx
@@ -0,0 +1,19 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Stack, useLocalSearchParams } from 'expo-router';
+
+const productDetailScreen = () => {
+
+  const { id } = useLocalSearchParams();
+  return (
+    <View>
+    <Stack.Screen options={{ title:'Details' + " " + id}} />
+
+      <Text>productDetailScreen {id}</Text>
+    </View>
+  )
+}
+
+export default productDetailScreen
+
+//
